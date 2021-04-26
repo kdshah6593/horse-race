9039805042a0ca6aafa3375bbded53051805a70e
Kunal <kdshah6593@gmail.com>
2021-04-26 15:55:19 -0400

    connect wagers component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import Track from './components/Track'
+import Wagers from './components/Wagers'
 
-function App() {
+const defHandStyle = {
+  maxHeight:'34vh',
+  minHeight:'34vh',
+  
+  maxWidth:'100vw',
+  padding: 0,
+};
+
+
+const App = () => {
   return (
     <div className="App">
       <h1>Horse Race</h1>
       <Track />
+      <Wagers />
     </div>
   );
 }
