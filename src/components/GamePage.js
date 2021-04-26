a11c135a98d3e3c92ffb5527d7b14a2422717ba0
Kunal <kdshah6593@gmail.com>
2021-04-26 16:14:27 -0400

    moved game to component, testing deck of cards api

---
+++ src/components/GamePage.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import Track from './Track'
+import Wagers from './Wagers'
+
+const GamePage = () => {
+    const [deck, setDeck] = useState({});
+    const [deckOfCards, setDeckOfCards] = useState([]);
+
+    useEffect(() =>{
+        fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+    })
+    
+    
+    return (
+        <div>
+            <h1>Horse Race</h1>
+            <Track />
+            <Wagers />
+        </div>
+    )
+}
+
+export default GamePage;
